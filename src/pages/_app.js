253838873aca6e603b996b0fb4ebe466ac438d4b
Guillermo Bergesio <91171763+GBergesio@users.noreply.github.com>
2023-01-27 13:11:47 -0300

    t3

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,16 @@
-import '@/styles/globals.css'
-import React from 'react'
-import ReactDOM from 'react-dom'
+// import '@/styles/globals.css'
+// import React from 'react'
+// import ReactDOM from 'react-dom'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// export default function App({ Component, pageProps }) {
+//   return <Component {...pageProps} />
+// }
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
+
+export default MyApp;
